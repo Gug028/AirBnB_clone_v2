b54d0bdbf3ec92d51dd8f9b67819ed606092713a
Gug028 <gramthembu@gmail.com>
2024-04-22 06:50:48 -0700

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application that listens on 0.0.0.0. port 5000
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    Display "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Display "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    Display "C " followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+    return "C {}".format(escape(text).replace('_', ' '))
+
+
+@app.route("/python/", default={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """
+    Display "Python " followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    """
+    return "Python {}".format(escape(text).replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    Display "n is a number" only if n is an integer
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
